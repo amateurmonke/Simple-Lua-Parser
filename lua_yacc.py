17c8357824440a957046d77f012bd0ba101b0e08
Prana-vvb <pvb1532005@gmail.com>
2024-11-16 19:38:29 +0530

    feat: Added function parsing

--- lua_yacc.py
+++ lua_yacc.py
@@ -27,15 +27,6 @@ def p_expression_identifier_assign(p):
         p[0] = f"{p[1]} = {p[3]}"
 
 
-# def p_identifier_list(p):
-#     """identifier_list : identifier_list COMMA ID
-#                        | ID"""
-#     if len(p) == 2:
-#         p[0] = p[1]
-#     else:
-#         p[0] = f"{p[1]}, {p[3]}"
-
-
 def p_expression_array(p):
     """expression : ID ASSIGN LCURLY expression_list RCURLY
                   | LOCAL ID ASSIGN LCURLY expression_list RCURLY"""
@@ -75,9 +66,22 @@ def p_prop_list(p):
         p[0] = f"{p[1]}, {p[3]}"
 
 
-# def p_expression_function(p):
-#     """expression : FUNCTION ID LPAREN RPAREN END"""
-#     p[0] = f"function {p[2]}() end"
+def p_identifier_list(p):
+    """identifier_list : identifier_list COMMA ID
+                       | ID"""
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = f"{p[1]}, {p[3]}"
+
+
+def p_expression_function(p):
+    """expression : FUNCTION ID LPAREN RPAREN END
+                  | FUNCTION ID LPAREN identifier_list RPAREN END"""
+    if len(p) == 6:
+        p[0] = f"function {p[2]}() end"
+    else:
+        p[0] = f"function {p[2]}({p[4]}) end"
 
 
 def p_error(p):
